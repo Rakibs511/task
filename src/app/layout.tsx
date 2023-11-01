1d7c14308495af05e83184d94303cbf57b93f8eb
Rakibul Hasan <rakib@localhost.localdomain>
2023-11-02 03:37:51 +0600

    Project Initialization

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Task || Application",
+  description:
+    "enter a name in the input box and pick the Sectors you are currently involved in.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
